ca2530b49b24aab47633379ddbdb2bff4fdb34f9
Vav <vav@zeywin.com>
2026-07-28 06:43:17 -0500

    Bump the SDK version

--- Runtime/Core/Models.cs
+++ Runtime/Core/Models.cs
@@ -357,7 +357,7 @@ namespace ZeyWinAds.Core
     /// </summary>
     public static class ZeyWinAdsConfig
     {
-        public const string SdkVersion = "3.9.43";
+        public const string SdkVersion = "3.9.44";
         public const int DefaultRewardAmount = 1;
         public const float RequestTimeoutSeconds = 30f;
         public const int MaxRetries = 2;
